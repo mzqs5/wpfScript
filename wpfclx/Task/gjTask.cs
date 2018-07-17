7afa43e36b9477ebb5852010fbaab02b9fe6b20c
李岳武 <2353131108@qq.com>
2018-07-18 02:09:29 +0800

    11

--- wpfclx/Task/gjTask.cs
+++ wpfclx/Task/gjTask.cs
@@ -19,14 +19,14 @@ namespace wpfclx.Task
 
         public override void Start(TaskModel model)
         {
-            Bg.Ocr(handle);
+            //Bg.Ocr(handle);
             //var r = Bg.FindPic(handle, Resource1.副本退出, new XRECT() { Left = 1144, Top = 180, Right = 1181, Bottom = 220 }, FindDirection.LeftTopToRightDown, 0.9f, true);
             //if (!r.IsEmpty)
             //{
             //    Bg.SetWindowText(handle, r.ToString());
             //    Thread.Sleep(10000);
             //}
-            //Bg.ExitWindowsEx();
+            Bg.ExitWindowsEx();
         }
     }
 }
