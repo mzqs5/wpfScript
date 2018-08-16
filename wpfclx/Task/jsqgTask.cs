3b7afd1fb67f8cbf87cc242f92fef16cee9cce47
李岳武 <2353131108@qq.com>
2018-08-16 11:31:05 +0800

    11

--- wpfclx/Task/jsqgTask.cs
+++ wpfclx/Task/jsqgTask.cs
@@ -32,8 +32,7 @@ namespace wpfclx.Task
                 while (true)
                 {
                     Bg.LeftMouseClick(handle, new Point() { X = 235, Y = 260 });
-                    Thread.Sleep(100);
-                    Bg.SetWindowText(handle, "检测到关注物品上架，开始抢购...");
+                    Thread.Sleep(130);
                     Bg.LeftMouseClick(handle, new Point() { X = 543, Y = 271 });
                     for (int i = 1; i < model.qgCount; i++)
                     {
@@ -41,7 +40,7 @@ namespace wpfclx.Task
                     }
                     Bg.LeftMouseClick(handle, new Point() { X = 659, Y = 526 });
                     Bg.LeftMouseClick(handle, new Point() { X = 885, Y = 528 });
-                    Thread.Sleep(120);
+                    Thread.Sleep(80);
                 }
             }
         }
